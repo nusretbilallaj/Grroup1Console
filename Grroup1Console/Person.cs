1870de5e3d8b496f3c4f51b9f20ca945da431b68
Nusret Bilallaj <netktit@gmail.com>
2022-10-31 19:00:48 +0100

    Shtimi i klases person

---
+++ Grroup1Console/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grroup1Console
+{
+    internal class Person
+    {
+
+        public Person(string emri,string mbiemri, string adresa)
+        {
+            Emri = emri;
+            Mbiemri = mbiemri;
+            Adresa = adresa;
+        }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+        public string Adresa { get; set; }
+    }
+}
